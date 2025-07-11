abb3d10fb1086fb87ebe0035ae9406e026709ad9
marcp20 <152832279+marcp20@users.noreply.github.com>
2025-07-11 15:09:16 +0630

    Update App.js

--- admin/src/App.js
+++ admin/src/App.js
@@ -3,7 +3,7 @@ import Footer from "./Components/Footer/Footer";
 import Navbar from "./Components/Navbar/Navbar";
 import Admin from "./Pages/Admin";
 
-export const backend_url = 'http://localhost:4000';
+export const backend_url = 'https://sneaker-website-1.onrender.com';
 export const currency = '$';
 
 function App() {
